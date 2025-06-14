f647ae7afc71d5eb612a6180c3e6955f38661d50
TheWiseKing05 <adityagangboir05@gmail.com>
2025-06-15 02:29:11 +0530

    Changes

--- app.py
+++ app.py
@@ -1,59 +1,59 @@
+import os
+import gdown
 import requests
 import streamlit as st
 import pickle
 import pandas as pd
 
-def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+# === Google Drive file URLs ===
+movies_dict_url = "https://drive.google.com/uc?id=1CishRUVNw4h--ffoIblUfpg-p1F5m57c"
+similarity_url = "https://drive.google.com/uc?id=1tH8Z4HkjoOI5n8n3ixO1L7ylAofLB5UD"
 
-def recommend(movie):
-    
-    movie_index = movies[movies['title'] == movie].index[0]
-    distances = similarity[movie_index]
-    movie_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+# === Download the .pkl files if not already present ===
+if not os.path.exists("movies_dict.pkl"):
+    gdown.download(movies_dict_url, "movies_dict.pkl", quiet=False)
 
-    recommendation_movies = []
-    recommendation_movies_posters = []
-    for i in movie_list:
-        movie_id = movies.iloc[i[0]].movie_id
-        # fetch poster from API
-        recommendation_movies.append(movies.iloc[i[0]].title)
-        recommendation_movies_posters.append(fetch_poster(movie_id))
-    return recommendation_movies,recommendation_movies_posters
+if not os.path.exists("similarity.pkl"):
+    gdown.download(similarity_url, "similarity.pkl", quiet=False)
 
+# === Load the pickled files ===
 movies_dict = pickle.load(open('movies_dict.pkl','rb'))
 movies = pd.DataFrame(movies_dict)
-
 similarity = pickle.load(open('similarity.pkl','rb'))
 
-st.title("Movie Recommender System")
+# === Poster fetcher ===
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+    data = requests.get(url).json()
+    poster_path = data.get('poster_path')
+    return f"https://image.tmdb.org/t/p/w500/{poster_path}" if poster_path else ""
+
+# === Recommender function ===
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_titles = []
+    recommended_posters = []
+    for i in movie_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_titles.append(movies.iloc[i[0]].title)
+        recommended_posters.append(fetch_poster(movie_id))
+    return recommended_titles, recommended_posters
+
+# === Streamlit UI ===
+st.title("🎬 CineMind: Movie Recommender")
 
 selected_movie_name = st.selectbox(
-    'How would you like to be contacted?',
+    'Select a movie you like:',
     movies['title'].values
 )
 
 if st.button('Recommend'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie_name)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
-
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+    names, posters = recommend(selected_movie_name)
+    cols = st.columns(5)
+    for idx, col in enumerate(cols):
+        with col:
+            st.text(names[idx])
+            st.image(posters[idx])
